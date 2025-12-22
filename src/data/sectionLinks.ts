17ae690e2bef35be2e90a6e3749b20f4e18cf134
neophiles <mnv.tagle@gmail.com>
2025-12-22 21:59:18 +0800

    feat: Add sectionLinks data

---
+++ src/data/sectionLinks.ts
@@ -0,0 +1,10 @@
+export type SectionLink = [
+  displayName: string, id: string
+]
+
+export const sectionLinks: SectionLink[] = [
+  ["About Me", "#hero"],
+  ["Tech Stack", "#tech-stack"],
+  ["Projects", "#projects"],
+  ["Achievements", "#achievements"],
+]
