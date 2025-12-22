f670f3d56e0be0adfc87400dd0a91c7c122cd965
neophiles <mnv.tagle@gmail.com>
2025-12-22 21:55:58 +0800

    feat: Add section nav links

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useThemeStore } from '../store/themeStore';
+import { sectionLinks } from '../data/sectionLinks';
 import { Sun, Moon } from 'lucide-react';
 import neophilesLogo from '../assets/logos/personal/neophiles-logo.png';
 
@@ -16,19 +17,39 @@ function Header() {
     }
   }, [isDark]);
 
+  const handleGoToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+
   return (
     <header className='h-14 w-full sticky top-0 z-10 px-6 dark:bg-gray-950/50 backdrop-blur-lg'>
       <div className='h-full flex items-center justify-between'>
-        <div className='flex items-center gap-2'>
-          <img src={neophilesLogo} alt='neophiles Logo' className='rounded-full w-8 h-8' />
-          <h1 className='text-lg'>
-            <code>neophiles</code>
-          </h1>
+        <div>
+          <button onClick={handleGoToTop} className='flex items-center gap-2'>
+            <img src={neophilesLogo} alt='neophiles Logo' className='rounded-full w-8 h-8' />
+            <h1 className='text-lg'>
+              <code>neophiles</code>
+            </h1>
+          </button>
         </div>
-          
-        <button onClick={() => setIsDark()} className='cursor-pointer text-yellow-600 dark:text-yellow-400'>
-          { !isDark ? <Sun /> : <Moon /> }
-        </button>
+
+        <div className='flex gap-8'>
+          <div className='flex items-center gap-4'>
+            {
+              sectionLinks.map(([displayName, id]) => (
+                <a key={id} href={id} className='text-md hover:text-blue-800 dark:hover:text-blue-400 transition-colors duration-200'>
+                  {displayName}
+                </a>
+              ))
+            }
+          </div>
+
+          <button onClick={() => setIsDark()} className='cursor-pointer text-yellow-600 dark:text-yellow-400'>
+            { !isDark ? <Sun /> : <Moon /> }
+          </button>
+        </div>
+        
       </div>
     </header>
   )
