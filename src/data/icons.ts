dd44743063eaf86376a501b2daa08f4539bc0517
neophiles <mnv.tagle@gmail.com>
2025-12-22 23:09:32 +0800

    chore: Delete icons.ts

--- src/data/icons.ts
+++
@@ -1,4 +0,0 @@
-import type { ForwardRefExoticComponent, RefAttributes } from 'react'
-import type { LucideProps } from 'lucide-react'
-
-export type LucideIcon = ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
