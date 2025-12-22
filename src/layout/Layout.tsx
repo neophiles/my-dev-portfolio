39754eb01c0e1435260645767907512a6c14838c
neophiles <mnv.tagle@gmail.com>
2025-12-22 21:56:54 +0800

    refactor: Remove gap-16 class

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -9,7 +9,7 @@ function Layout() {
   return (
     <>
       <Header />
-      <main className='w-[min(900px,99%)] flex flex-col gap-16 p-6 '>
+      <main className='w-[min(900px,99%)] flex flex-col p-6'>
         <Hero />
         <TechStack />
         <Projects />
